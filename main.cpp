f7d68aabc8fe8921cc46dc0a9e11688ef29106ce
Pranit Bauva <pranit.bauva@gmail.com>
2016-09-30 10:34:00 +0530

    Revert "remove contours code"
    This reverts commit 644f8ea2e2227e5e3a00c982220e3e648fd9dd82.

--- main.cpp
+++ main.cpp
@@ -215,6 +215,11 @@ int main(int argc, const char **argv) {
         }
 
         check_status();
+        yellow_drawing = Mat::zeros(src.size(), CV_8UC3);
+        imshow("Yellow Drawing", yellow_drawing);
+
+        //createTrackbar("Threshold: ", "Source", &binary_thresh, 255, thresh_callback);
+        //thresh_callback(0, 0);
         waitKey(70);
     }
 
@@ -450,4 +455,159 @@ void merge_obstacle_image() {
 }
 
 void thresh_callback(int, void *) {
+    Mat threshold_output;
+    vector<vector<Point> > head_contours, tail_contours, yellow_contours, green_contours, blue_contours, brown_contours, red_contours;
+    vector<Vec4i> head_hierarchy, tail_hierarchy, yellow_hierarchy, green_hierarchy, blue_hierarchy, brown_hierarchy, red_hierarchy;
+
+    /* Detect edges in all using Threshold */
+    threshold(head_img, head_img_gray, 100, 255, THRESH_BINARY);
+    threshold(tail_img, tail_img_gray, 100, 255, THRESH_BINARY);
+    threshold(yellow_img, yellow_img_gray, 100, 255, THRESH_BINARY);
+    threshold(green_img, green_img_gray, 100, 255, THRESH_BINARY);
+    threshold(brown_img, brown_img_gray, 100, 255, THRESH_BINARY);
+    threshold(red_img, red_img_gray, 100, 255, THRESH_BINARY);
+
+    /* Find contours */
+    findContours(head_img_gray, head_contours, head_hierarchy, CV_RETR_TREE,
+                 CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    findContours(tail_img_gray, tail_contours, tail_hierarchy, CV_RETR_TREE,
+                 CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    findContours(yellow_img_gray, yellow_contours, yellow_hierarchy,
+                 CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    findContours(green_img_gray, green_contours, green_hierarchy,
+                 CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    findContours(brown_img_gray, brown_contours, brown_hierarchy,
+                 CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    findContours(red_img_gray, red_contours, red_hierarchy,
+                 CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+    vector<vector<Point> > head_contours_poly(head_contours.size());
+    vector<vector<Point> > tail_contours_poly(tail_contours.size());
+    vector<vector<Point> > yellow_contours_poly(yellow_contours.size());
+    vector<vector<Point> > green_contours_poly(green_contours.size());
+    vector<vector<Point> > blue_contours_poly(blue_contours.size());
+    vector<vector<Point> > brown_contours_poly(brown_contours.size());
+    vector<vector<Point> > red_contours_poly(red_contours.size());
+
+    vector<Rect> headBoundRect(head_contours.size());
+    vector<Rect> tailBoundRect(tail_contours.size());
+    vector<Rect> yellowBoundRect(yellow_contours.size());
+    vector<Rect> greenBoundRect(green_contours.size());
+    vector<Rect> blueBoundRect(blue_contours.size());
+    vector<Rect> brownBoundRect(brown_contours.size());
+    vector<Rect> redBoundRect(red_contours.size());
+
+    for (int i = 0; i < head_contours.size(); i++) {
+        approxPolyDP(Mat(head_contours[i]), head_contours_poly[i], 3, true);
+        headBoundRect[i] = boundingRect(Mat(head_contours_poly[i]));
+    }
+
+    for (int i = 0; i < tail_contours.size(); i++) {
+        approxPolyDP(Mat(tail_contours[i]), tail_contours_poly[i], 3, true);
+        tailBoundRect[i] = boundingRect(Mat(tail_contours_poly[i]));
+    }
+
+    for (int i = 0; i < yellow_contours.size(); i++) {
+        approxPolyDP(Mat(yellow_contours[i]), yellow_contours_poly[i], 3, true);
+        yellowBoundRect[i] = boundingRect(Mat(yellow_contours_poly[i]));
+        printf("%d\n", i);
+    }
+
+    for (int i = 0; i < green_contours.size(); i++) {
+        approxPolyDP(Mat(green_contours[i]), green_contours_poly[i], 3, true);
+        greenBoundRect[i] = boundingRect(Mat(green_contours_poly[i]));
+    }
+
+    for (int i = 0; i < blue_contours.size(); i++) {
+        approxPolyDP(Mat(blue_contours[i]), blue_contours_poly[i], 3, true);
+        blueBoundRect[i] = boundingRect(Mat(blue_contours_poly[i]));
+    }
+
+    for (int i = 0; i < brown_contours.size(); i++) {
+        approxPolyDP(Mat(brown_contours[i]), brown_contours_poly[i], 3, true);
+        brownBoundRect[i] = boundingRect(Mat(brown_contours_poly[i]));
+    }
+
+    for (int i = 0; i < red_contours.size(); i++) {
+        approxPolyDP(Mat(red_contours[i]), red_contours_poly[i], 3, true);
+        redBoundRect[i] = boundingRect(Mat(red_contours_poly[i]));
+    }
+
+    head_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
+    tail_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
+    green_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
+    blue_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
+    brown_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
+    red_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
+
+    for (int i = 0; i < head_contours.size(); i++) {
+        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        drawContours(head_drawing, head_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+        rectangle(head_drawing, headBoundRect[i].tl(), headBoundRect[i].br(), color, 2, 8, 0);
+    }
+    for (int i = 0; i < tail_contours.size(); i++) {
+        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        drawContours(tail_drawing, tail_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+        rectangle(tail_drawing, tailBoundRect[i].tl(), tailBoundRect[i].br(), color, 2, 8, 0);
+    }
+    for (int i = 0; i < yellow_contours.size(); i++) {
+        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        drawContours(yellow_drawing, yellow_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+        rectangle(yellow_drawing, yellowBoundRect[i].tl(), yellowBoundRect[i].br(), color, 2, 8, 0);
+    }
+    for (int i = 0; i < green_contours.size(); i++) {
+        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        drawContours(green_drawing, green_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+        rectangle(green_drawing, greenBoundRect[i].tl(), greenBoundRect[i].br(), color, 2, 8, 0);
+    }
+    for (int i = 0; i < blue_contours.size(); i++) {
+        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        drawContours(blue_drawing, blue_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+        rectangle(blue_drawing, blueBoundRect[i].tl(), blueBoundRect[i].br(), color, 2, 8, 0);
+    }
+    for (int i = 0; i < red_contours.size(); i++) {
+        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        drawContours(red_drawing, red_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+        rectangle(red_drawing, redBoundRect[i].tl(), redBoundRect[i].br(), color, 2, 8, 0);
+    }
+    for (int i = 0; i < brown_contours.size(); i++) {
+        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        drawContours(brown_drawing, brown_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+        rectangle(brown_drawing, brownBoundRect[i].tl(), brownBoundRect[i].br(), color, 2, 8, 0);
+    }
+
+    imshow("Head Contours", head_drawing);
+    imshow("Tail Contours", tail_drawing);
+    imshow("Yellow Contours", yellow_drawing);
+    imshow("Green Contours", green_drawing);
+    imshow("Blue Contours", blue_drawing);
+    imshow("Brown Contours", brown_drawing);
+    imshow("Red Contours", red_drawing);
+
+
+    vector<vector<Point> > food;
+    vector<vector<Point> > wood;
+    vector<vector<Point> > gold;
+    vector<vector<Point> > stone;
+    vector<vector<Point> > obstacles;
+
+    for (int i = 0; i < yellow_contours_poly.size(); i++) {
+        if (yellow_contours_poly[i].size() == 3)
+            stone.push_back(yellow_contours_poly[i]);
+        if (yellow_contours_poly[i].size() == 4)
+            gold.push_back(yellow_contours_poly[i]);
+    }
+    for (int i = 0; i < green_contours_poly.size(); i++) {
+        if (green_contours_poly[i].size() == 3)
+            stone.push_back(green_contours_poly[i]);
+        if (green_contours_poly[i].size() == 4)
+            wood.push_back(green_contours_poly[i]);
+    }
+    for (int i = 0; i < blue_contours_poly.size(); i++) {
+        obstacles.push_back(blue_contours_poly[i]);
+    }
+    for (int i = 0; i < brown_contours_poly.size(); i++) {
+        obstacles.push_back(brown_contours_poly[i]);
+    }
+
 }
