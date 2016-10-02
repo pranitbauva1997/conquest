d40197a7d8152060041228548794ca6107446acb
Pranit Bauva <pranit.bauva@gmail.com>
2016-10-02 16:28:38 +0530

    detect contours PROPERLY! yay!
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ using namespace cv;
 using namespace std;
 
 Mat src, src_gray, imgHSV, yellow_img, green_img, blue_img, brown_img, red_img, head_img, tail_img;
-Mat yellow_img_gray, green_img_gray, blue_img_gray, brown_img_gray, red_img_gray, head_img_gray, tail_img_gray;
 Mat yellow_img_poly, green_img_poly, blue_img_poly, brown_img_poly, red_img_poly, head_img_poly, tail_img_poly;
 Mat yellow_drawing, green_drawing, blue_drawing, brown_drawing, red_drawing, head_drawing, tail_drawing;
 
 int binary_thresh = 100;
+int init = 0;
 
 /* Path planning variables */
 enum start_point {
@@ -66,10 +66,8 @@ void thresh_callback(int, void *);
 void init_trackbars() {
     namedWindow("Bot Trackbars", 1);
     namedWindow("Yellow Trackbars", 1);
-    namedWindow("Green Trackbars", 1);
     namedWindow("Blue Trackbars", 1);
     namedWindow("Brown Trackbars", 1);
-    namedWindow("Red Trackbars", 1);
     createTrackbar("H1 Head", "Bot Trackbars", &head.h_low, 255);
     createTrackbar("H2 Head", "Bot Trackbars", &head.h_high, 255);
     createTrackbar("S1 Head", "Bot Trackbars", &head.s_low, 255);
@@ -88,12 +86,6 @@ void init_trackbars() {
     createTrackbar("S2 Yellow", "Yellow Trackbars", &yellow.s_high, 255);
     createTrackbar("V1 Yellow", "Yellow Trackbars", &yellow.v_low, 255);
     createTrackbar("V2 Yellow", "Yellow Trackbars", &yellow.v_high, 255);
-    createTrackbar("H1 Green", "Green Trackbars", &green.h_low, 255);
-    createTrackbar("H2 Green", "Green Trackbars", &green.h_high, 255);
-    createTrackbar("S1 Green", "Green Trackbars", &green.s_low, 255);
-    createTrackbar("S2 Green", "Green Trackbars", &green.s_high, 255);
-    createTrackbar("V1 Green", "Green Trackbars", &green.v_low, 255);
-    createTrackbar("V2 Green", "Green Trackbars", &green.v_high, 255);
     createTrackbar("H1 Blue", "Blue Trackbars", &blue.h_low, 255);
     createTrackbar("H2 Blue", "Blue Trackbars", &blue.h_high, 255);
     createTrackbar("S1 Blue", "Blue Trackbars", &blue.s_low, 255);
@@ -106,20 +98,14 @@ void init_trackbars() {
     createTrackbar("S2 Brown", "Brown Trackbars", &brown.s_high, 255);
     createTrackbar("V1 Brown", "Brown Trackbars", &brown.v_low, 255);
     createTrackbar("V2 Brown", "Brown Trackbars", &brown.v_high, 255);
-    createTrackbar("H1 Red", "Red Trackbars", &red.h_low, 255);
-    createTrackbar("H2 Red", "Red Trackbars", &red.h_high, 255);
-    createTrackbar("S1 Red", "Red Trackbars", &red.s_low, 255);
-    createTrackbar("S2 Red", "Red Trackbars", &red.s_high, 255);
-    createTrackbar("V1 Red", "Red Trackbars", &red.v_low, 255);
-    createTrackbar("V2 Red", "Red Trackbars", &red.v_high, 255);
 }
 
 void init_hsvcolor() {
-    yellow.h_low = 4;
-    yellow.h_high = 58;
-    yellow.s_low = 97;
+    yellow.h_low = 27;
+    yellow.h_high = 39;
+    yellow.s_low = 60;
     yellow.s_high = 255;
-    yellow.v_low = 0;
+    yellow.v_low = 234;
     yellow.v_high = 255;
     green.h_low = 0;
     green.h_high = 0;
@@ -127,32 +113,32 @@ void init_hsvcolor() {
     green.s_high = 0;
     green.v_low = 0;
     green.v_high = 0;
-    brown.h_low = 0;
-    brown.h_high = 17;
-    brown.s_low = 43;
-    brown.s_high = 94;
+    brown.h_low = 10;
+    brown.h_high = 16;
+    brown.s_low = 55;
+    brown.s_high = 85;
     brown.v_low = 188;
-    brown.v_high = 217;
+    brown.v_high = 210;
     red.h_low = 0;
     red.h_high = 0;
     red.s_low = 0;
     red.s_high = 0;
     red.v_low = 0;
     red.v_high = 0;
-    head.h_low = 0;
-    head.h_high = 0;
-    head.s_low = 0;
-    head.s_high = 0;
-    head.v_low = 0;
-    head.v_high = 0;
+    head.h_low = 109;
+    head.h_high = 139;
+    head.s_low = 66;
+    head.s_high = 97;
+    head.v_low = 183;
+    head.v_high = 255;
     tail.h_low = 0;
-    tail.h_high = 0;
+    tail.h_high = 13;
     tail.s_low = 0;
-    tail.s_high = 0;
-    tail.v_low = 0;
-    tail.v_high = 0;
-    blue.h_low = 47;
-    blue.h_high = 100;
+    tail.s_high = 182;
+    tail.v_low = 219;
+    tail.v_high = 255;
+    blue.h_low = 75;
+    blue.h_high = 85;
     blue.s_low = 0;
     blue.s_high = 18;
     blue.v_low = 235;
@@ -160,7 +146,7 @@ void init_hsvcolor() {
 }
 
 int main(int argc, const char **argv) {
-    VideoCapture cap(0);
+    VideoCapture cap(1);
 
     if (!cap.isOpened()) {
         fprintf(stderr, "ERROR!\n");
@@ -168,15 +154,13 @@ int main(int argc, const char **argv) {
     }
 
     init_hsvcolor();
-    init_trackbars();
+    //init_trackbars();
 
     while(1) {
         cap >> src;
         cvtColor(src, imgHSV, CV_BGR2HSV);
         inRange(imgHSV, Scalar(yellow.h_low, yellow.s_low, yellow.v_low),
                 Scalar(yellow.h_high, yellow.s_high, yellow.v_high), yellow_img);
-        inRange(imgHSV, Scalar(green.h_low, green.s_low, green.v_low),
-                Scalar(green.h_high, green.s_high, green.v_high), green_img);
         inRange(imgHSV, Scalar(blue.h_low, blue.s_low, blue.v_low),
                 Scalar(blue.h_high, blue.s_high, blue.v_high), blue_img);
         inRange(imgHSV, Scalar(brown.h_low, brown.s_low, brown.v_low),
@@ -189,21 +173,15 @@ int main(int argc, const char **argv) {
                 Scalar(tail.h_high, tail.s_high, tail.v_high), tail_img);
 
         imshow("Source", src);
-        imshow("Head", head_img);
-        imshow("Tail", tail_img);
-        imshow("Yellow", yellow_img);
-        imshow("Green", green_img);
-        imshow("Blue", blue_img);
-        imshow("Brown", brown_img);
-        imshow("Red", red_img);
+        //imshow("Head", head_img);
+        //imshow("Tail", tail_img);
+        //imshow("Yellow", yellow_img);
+        //imshow("Green", green_img);
+        //imshow("Blue", blue_img);
+        //imshow("Brown", brown_img);
+        //imshow("Red", red_img);
 
-        threshold_color_images();
-
-        // createTrackbar("Threshold: ", "Source", &binary_thresh, 255, thresh_callback);
-        // thresh_callback(0, 0);
-        merge_obstacle_image();
-
-        /* Compute Path and move bot's ass */
+        /* Compute Path and move bot's ass
         if (status == IN_BETWEEN_PATH) {
             continue_moving();
         } else if (status == START_POINT) {
@@ -214,18 +192,16 @@ int main(int argc, const char **argv) {
             status = IN_BETWEEN_PATH;
         }
 
-        check_status();
-        yellow_drawing = Mat::zeros(src.size(), CV_8UC3);
-        imshow("Yellow Drawing", yellow_drawing);
+        check_status(); */
 
-        //createTrackbar("Threshold: ", "Source", &binary_thresh, 255, thresh_callback);
-        //thresh_callback(0, 0);
+        createTrackbar("Threshold: ", "Source", &binary_thresh, 255, thresh_callback);
+        thresh_callback(0, 0);
         waitKey(70);
     }
 
     return 0;
 }
-
+/*
 void init() {
     start_node.position.x = town_centre.x;
     start_node.position.y = town_centre.y;
@@ -379,7 +355,7 @@ void check_status() {
     }
 }
 
-int distance distance(Point a, Point b) {
+int distance(Point a, Point b) {
     Point temp;
     temp.x = a.x - b.x;
     temp.y = a.y - b.y;
@@ -394,91 +370,29 @@ void continue_moving() {
     if (x > 0 && y < 0)
         go_right();
 }
-
-void threshold_color_images() {
-    threshold_yellow_image();
-    threshold_green_image();
-    threshold_blue_image();
-    threshold_red_image();
-    threshold_brown_image();
-}
-
-void threshold_blue_image() {
-    for (int i = 0; i < blue_img.rows; i++) {
-        if (int j = 0; j < blue_img.cols; j++) {
-            if (blue_img.at<Vec3b>(i, j)[2] > 100)
-                blue_img_gray.at<uchar> = 255;
-            else
-                blue_image_gray.at<uchar> = 0;
-        }
-    }
-}
-
-void threshold_green_image() {
-    for (int i = 0; i < green_img.rows; i++) {
-        for (int j = 0; j < green_img.cols; j++) {
-            if (green_img.at<Vec3b>(i, j)[2] > 100)
-                green_img_gray.at<uchar> = 255;
-            else
-                green_img_gray.at<uchar> = 0;
-        }
-    }
-}
-
-void threshold_brown_image() {
-    for (int i = 0; i < brown_image.rows; i++) {
-        for (int j = 0; j < brown_image.cols; j++) {
-            if (brown_image.at<Vec3b>(i, j)[2] > 100)
-                brown_image_gray.at<uchar> = 255;
-            else
-                brown_image_gray.at<uchar> = 0;
-        }
-    }
-}
-
-void threshold_red_image() {
-    for (int i = 0; i < red_image.rows; i++) {
-        for (int j = 0; j < red_image.cols; j++) {
-            if (red_image.at<Vec3b>(i, j)[2] > 100)
-                red_image_gray.at<uchar> = 255;
-            else
-                red_image_gray.at<uchar> = 0;
-        }
-    }
-}
-
-void merge_obstacle_image() {
-    for (int i = 0; i < blue_img.rows; i++)
-        for (int j = 0; j < blue_img.cols; j++)
-            if (blue_img_gray.at<uchar>(i, j) == 255)
-                brown_img.at<Vec3b>(i, j) = 255;
-}
+*/
 
 void thresh_callback(int, void *) {
-    Mat threshold_output;
+    Mat yellow_output, blue_output, head_output, tail_output, brown_output;
     vector<vector<Point> > head_contours, tail_contours, yellow_contours, green_contours, blue_contours, brown_contours, red_contours;
     vector<Vec4i> head_hierarchy, tail_hierarchy, yellow_hierarchy, green_hierarchy, blue_hierarchy, brown_hierarchy, red_hierarchy;
 
-    /* Detect edges in all using Threshold */
-    threshold(head_img, head_img_gray, 100, 255, THRESH_BINARY);
-    threshold(tail_img, tail_img_gray, 100, 255, THRESH_BINARY);
-    threshold(yellow_img, yellow_img_gray, 100, 255, THRESH_BINARY);
-    threshold(green_img, green_img_gray, 100, 255, THRESH_BINARY);
-    threshold(brown_img, brown_img_gray, 100, 255, THRESH_BINARY);
-    threshold(red_img, red_img_gray, 100, 255, THRESH_BINARY);
+    threshold(yellow_img, yellow_output, binary_thresh, 255, THRESH_BINARY);
+    threshold(blue_img, blue_output, binary_thresh, 255, THRESH_BINARY);
+    threshold(head_img, head_output, binary_thresh, 255, THRESH_BINARY);
+    threshold(tail_img, tail_output, binary_thresh, 255, THRESH_BINARY);
+    threshold(brown_img, brown_output, binary_thresh, 255, THRESH_BINARY);
 
     /* Find contours */
-    findContours(head_img_gray, head_contours, head_hierarchy, CV_RETR_TREE,
+    findContours(head_output, head_contours, head_hierarchy, CV_RETR_TREE,
                  CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-    findContours(tail_img_gray, tail_contours, tail_hierarchy, CV_RETR_TREE,
+    findContours(tail_output, tail_contours, tail_hierarchy, CV_RETR_TREE,
                  CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-    findContours(yellow_img_gray, yellow_contours, yellow_hierarchy,
+    findContours(yellow_output, yellow_contours, yellow_hierarchy,
                  CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-    findContours(green_img_gray, green_contours, green_hierarchy,
+    findContours(brown_output, brown_contours, brown_hierarchy,
                  CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-    findContours(brown_img_gray, brown_contours, brown_hierarchy,
-                 CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-    findContours(red_img_gray, red_contours, red_hierarchy,
+    findContours(blue_output, blue_contours, blue_hierarchy,
                  CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
     vector<vector<Point> > head_contours_poly(head_contours.size());
@@ -499,115 +413,105 @@ void thresh_callback(int, void *) {
 
     for (int i = 0; i < head_contours.size(); i++) {
         approxPolyDP(Mat(head_contours[i]), head_contours_poly[i], 3, true);
-        headBoundRect[i] = boundingRect(Mat(head_contours_poly[i]));
+        if (contourArea(head_contours[i]) > 100)
+            headBoundRect[i] = boundingRect(Mat(head_contours_poly[i]));
     }
 
     for (int i = 0; i < tail_contours.size(); i++) {
         approxPolyDP(Mat(tail_contours[i]), tail_contours_poly[i], 3, true);
-        tailBoundRect[i] = boundingRect(Mat(tail_contours_poly[i]));
+        if (contourArea(tail_contours[i]) > 500)
+            tailBoundRect[i] = boundingRect(Mat(tail_contours_poly[i]));
     }
 
     for (int i = 0; i < yellow_contours.size(); i++) {
         approxPolyDP(Mat(yellow_contours[i]), yellow_contours_poly[i], 3, true);
-        yellowBoundRect[i] = boundingRect(Mat(yellow_contours_poly[i]));
-        printf("%d\n", i);
+        if (contourArea(yellow_contours[i]) > 100)
+            yellowBoundRect[i] = boundingRect(Mat(yellow_contours_poly[i]));
     }
 
     for (int i = 0; i < green_contours.size(); i++) {
         approxPolyDP(Mat(green_contours[i]), green_contours_poly[i], 3, true);
-        greenBoundRect[i] = boundingRect(Mat(green_contours_poly[i]));
+        if (contourArea(green_contours[i]) > 100)
+            greenBoundRect[i] = boundingRect(Mat(green_contours_poly[i]));
     }
 
     for (int i = 0; i < blue_contours.size(); i++) {
         approxPolyDP(Mat(blue_contours[i]), blue_contours_poly[i], 3, true);
-        blueBoundRect[i] = boundingRect(Mat(blue_contours_poly[i]));
+        if (contourArea(blue_contours[i]))
+            blueBoundRect[i] = boundingRect(Mat(blue_contours_poly[i]));
     }
 
     for (int i = 0; i < brown_contours.size(); i++) {
         approxPolyDP(Mat(brown_contours[i]), brown_contours_poly[i], 3, true);
-        brownBoundRect[i] = boundingRect(Mat(brown_contours_poly[i]));
+        if (contourArea(brown_contours[i]) > 100)
+            brownBoundRect[i] = boundingRect(Mat(brown_contours_poly[i]));
     }
 
-    for (int i = 0; i < red_contours.size(); i++) {
-        approxPolyDP(Mat(red_contours[i]), red_contours_poly[i], 3, true);
-        redBoundRect[i] = boundingRect(Mat(red_contours_poly[i]));
-    }
-
-    head_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
-    tail_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
-    green_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
-    blue_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
-    brown_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
-    red_drawing = Mat::zeros(src_gray.size(), CV_8UC3);
+    head_drawing = Mat::zeros(src.size(), CV_8UC3);
+    tail_drawing = Mat::zeros(src.size(), CV_8UC3);
+    green_drawing = Mat::zeros(src.size(), CV_8UC3);
+    blue_drawing = Mat::zeros(src.size(), CV_8UC3);
+    brown_drawing = Mat::zeros(src.size(), CV_8UC3);
+    yellow_drawing = Mat::zeros(src.size(), CV_8UC3);
 
     for (int i = 0; i < head_contours.size(); i++) {
         Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        drawContours(head_drawing, head_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
         rectangle(head_drawing, headBoundRect[i].tl(), headBoundRect[i].br(), color, 2, 8, 0);
     }
     for (int i = 0; i < tail_contours.size(); i++) {
         Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        drawContours(tail_drawing, tail_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
         rectangle(tail_drawing, tailBoundRect[i].tl(), tailBoundRect[i].br(), color, 2, 8, 0);
     }
     for (int i = 0; i < yellow_contours.size(); i++) {
         Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        drawContours(yellow_drawing, yellow_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
         rectangle(yellow_drawing, yellowBoundRect[i].tl(), yellowBoundRect[i].br(), color, 2, 8, 0);
+        Point centre_rect = (yellowBoundRect[i].tl() + yellowBoundRect[i].br());
+        centre_rect.x /= 2;
+        centre_rect.y /= 2;
+        if (!init)
+            target_resources.push_back(centre_rect);
     }
     for (int i = 0; i < green_contours.size(); i++) {
         Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        drawContours(green_drawing, green_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
         rectangle(green_drawing, greenBoundRect[i].tl(), greenBoundRect[i].br(), color, 2, 8, 0);
+        Point centre_rect = (greenBoundRect[i].tl() + greenBoundRect[i].br());
+        centre_rect.x /= 2;
+        centre_rect.y /= 2;
+        if (!init)
+            target_resources.push_back(centre_rect);
     }
     for (int i = 0; i < blue_contours.size(); i++) {
         Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        drawContours(blue_drawing, blue_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
         rectangle(blue_drawing, blueBoundRect[i].tl(), blueBoundRect[i].br(), color, 2, 8, 0);
+        Point centre_rect = (blueBoundRect[i].tl() + blueBoundRect[i].br());
+        centre_rect.x /= 2;
+        centre_rect.y /= 2;
+        if (!init)
+            target_resources.push_back(centre_rect);
     }
     for (int i = 0; i < red_contours.size(); i++) {
         Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        drawContours(red_drawing, red_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
         rectangle(red_drawing, redBoundRect[i].tl(), redBoundRect[i].br(), color, 2, 8, 0);
+        Point centre_rect = (redBoundRect[i].tl() + redBoundRect[i].br());
+        centre_rect.x /= 2;
+        centre_rect.y /= 2;
+        if (!init)
+            target_resources.push_back(centre_rect);
     }
     for (int i = 0; i < brown_contours.size(); i++) {
         Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        drawContours(brown_drawing, brown_contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
         rectangle(brown_drawing, brownBoundRect[i].tl(), brownBoundRect[i].br(), color, 2, 8, 0);
+        Point centre_rect = (brownBoundRect[i].tl() + brownBoundRect[i].br());
+        centre_rect.x /= 2;
+        centre_rect.y /= 2;
+        if (!init)
+            target_resources.push_back(centre_rect);
     }
+    init++;
 
     imshow("Head Contours", head_drawing);
     imshow("Tail Contours", tail_drawing);
     imshow("Yellow Contours", yellow_drawing);
-    imshow("Green Contours", green_drawing);
     imshow("Blue Contours", blue_drawing);
     imshow("Brown Contours", brown_drawing);
-    imshow("Red Contours", red_drawing);
-
-
-    vector<vector<Point> > food;
-    vector<vector<Point> > wood;
-    vector<vector<Point> > gold;
-    vector<vector<Point> > stone;
-    vector<vector<Point> > obstacles;
-
-    for (int i = 0; i < yellow_contours_poly.size(); i++) {
-        if (yellow_contours_poly[i].size() == 3)
-            stone.push_back(yellow_contours_poly[i]);
-        if (yellow_contours_poly[i].size() == 4)
-            gold.push_back(yellow_contours_poly[i]);
-    }
-    for (int i = 0; i < green_contours_poly.size(); i++) {
-        if (green_contours_poly[i].size() == 3)
-            stone.push_back(green_contours_poly[i]);
-        if (green_contours_poly[i].size() == 4)
-            wood.push_back(green_contours_poly[i]);
-    }
-    for (int i = 0; i < blue_contours_poly.size(); i++) {
-        obstacles.push_back(blue_contours_poly[i]);
-    }
-    for (int i = 0; i < brown_contours_poly.size(); i++) {
-        obstacles.push_back(brown_contours_poly[i]);
-    }
-
 }
